8872d4f9432ac4a54090bbf6534520e7fc2d7fa3
Anish Chowdhury <chowdhury.anish.2006@gmail.com>
2026-03-10 02:07:17 +0530

    fixing code runner fuckup

--- assignment.pl
+++ assignment.pl
@@ -1,5 +1,7 @@
 # write down a perl code that will print all the numbers from 1 to 50 which are divisible by 2 using for loop
 
+#!/usr/bin/perl
+
 for ($i = 1; $i <= 100; $i++)
 {
     if($i%4 == 0)
