df15b8bd5eebd46630d728a1fd4041f0dcc73ef0
Anish Chowdhury <chowdhury.anish.2006@gmail.com>
2026-03-10 13:28:00 +0530

    variables and print fnx

--- main.pl
+++ main.pl
@@ -1,8 +1,12 @@
 
 #!/usr/bin/perl
-print "Hello, 
+print "Hello, world\n";
 
-    world\n";
+$a = 56;
+print "Value of a is $a \n";
+print 'Value of a is $a \n';
 
-
+$name = "anish";
+print "\nMy name is $name\n";
+exit;
 
