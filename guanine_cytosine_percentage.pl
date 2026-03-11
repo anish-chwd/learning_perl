e97cbd2fd0bdb5636d481fa5708ac7865649c611
anish-chwd <245670617+anish-chwd@users.noreply.github.com>
2026-03-11 10:55:57 +0530

    GC%

---
+++ guanine_cytosine_percentage.pl
@@ -0,0 +1,38 @@
+#find GC%
+ 
+#!/usr/local/bin/perl
+ 
+print "Enter file name : ";
+$file = <STDIN>;
+chomp($file);
+open(DNAINFILE,$file);
+@nucleotide = <DNAINFILE>;
+close DNAINFILE;
+$nucleotide = join('',@nucleotide);
+$nucleotide =~ s/\s//g;
+@nucleotide = split('', $nucleotide);
+#print "@nucleotide";
+
+$a = 0;
+$g = 0;
+$t = 0;
+$c = 0;
+foreach $base(@nucleotide){
+    if($base eq 'A'){
+        $a++;
+    }elsif($base eq 'G'){
+        $g++;
+    }elsif($base eq 'T'){
+        $t++;
+    }elsif($base eq 'C'){
+        $c++;
+    }
+}
+
+#GC% 
+$total = $a+$g+$t+$c;
+$gc = $g + $c;
+$gc_percent = (($gc/$total)*100);
+print "The GC % is : $gc_percent %\n";
+ 
+exit;
